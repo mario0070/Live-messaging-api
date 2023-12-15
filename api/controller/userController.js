22daf3a901389dd1b08d279802540eddf3c8882f
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2023-12-15 08:43:55 +0100

    user login

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -18,19 +18,43 @@ const createUser = (req, res) => {
         if(data.length >= 1){
             res.status(402).json({message : "user already exist"})
         }else{
-            const user = new userSchema({
-                firstname : req.body.firstname,
-                email : req.body.email,
-                password : req.body.password,
-                username : req.body.username,
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                const user = new userSchema({
+                    firstname : req.body.firstname,
+                    email : req.body.email,
+                    password : hash,
+                    username : req.body.username,
+                })
+                user.save()
+                .then(data => {
+                    res.status(200).json({data})
+                })
+                .catch(err => {
+                    res.status(500).json({err})
+                }) 
             })
-            user.save()
-            .then(data => {
-                res.status(200).json({data})
+           
+        }
+    })
+    .catch(err => {
+        res.status(500).json({err})
+    })
+}
+
+const loginUser = (req, res) => {
+    userSchema.find({email : req.body.email})
+    .then(data => {
+        if(data.length <= 0){
+            res.status(403).json({message : "user does not exist"})
+        }else{
+            bcrypt.compare(req.body.password, data[0].password, (err, hash) => {
+                if(hash){
+                    res.json(data)
+                }else{
+                    res.status(403).json({message : "invalid credential"})
+                }
             })
-            .catch(err => {
-                res.status(500).json({err})
-            }) 
+           
         }
     })
     .catch(err => {
@@ -41,4 +65,5 @@ const createUser = (req, res) => {
 module.exports = {
     getUser,
     createUser,
+    loginUser,
 }
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,5 +4,6 @@ const userController = require("../controller/userController")
 
 route.get("/", userController.getUser)
 route.post("/", userController.createUser)
+route.post("/login", userController.loginUser)
 
 module.exports = route
