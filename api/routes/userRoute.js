fb904e4dd3941a1b7bec09dbc7647371af9de3b6
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2023-12-15 13:11:58 +0100

    protecting route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const route = express.Router()
 const userController = require("../controller/userController")
+const checkAuth = require("../middleware/checkAuth")
 
-route.get("/", userController.getUser)
+route.get("/",checkAuth, userController.getUser)
 route.post("/", userController.createUser)
 route.post("/login", userController.loginUser)
 
